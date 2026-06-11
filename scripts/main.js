d58d1630d836c756b733e468d25592cb79558c31
Rocio Gregorio <rociogregoriofunval@gmail.com>
2026-06-11 19:42:02 -0300

    proyectofinal filtro ciudad

--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,28 @@
  * Este bloque de código contiene la funcionalidad principal
  * que define el comportamiento del programa.
  */
-import { stays } from "./stays";
 async function capturarDatos() {
-    try{
+    try {
         let respuesta = await fetch("/stays.json");
         let data = await respuesta.json();
         return data;
-
-    } catch (error){
-        console.error(`${error}`)
+    } catch (error) {
+        console.error(`${error}`);
     }
 }
-let tarjet = await capturarDatos();
-// console.log(tarjet)
-let contenedorTarjeta = document.querySelector("#tarjeta");
 
-for (const tarjeta of tarjet){
-    let tarjetasGeneradas = "" ;
+let tarjet = await capturarDatos();
+let contenedorTarjeta = document.querySelector("#tarjeta");
+let btnAl = document.querySelector("#btnAl");
+let addL = document.querySelector("#addL");
+renderizarHtml(tarjet)
+
+function renderizarHtml(lista){
+    contenedorTarjeta.innerHTML=""
+    for (const tarjeta of lista){
+     
+        let tarjetasGeneradas = "" ;
+    
     tarjetasGeneradas+=`
             <div>
 
@@ -31,7 +36,7 @@ for (const tarjeta of tarjet){
             ? `<span class="border border-gray-400 text-gray-700 font-bold uppercase px-2 py-0.5 rounded-full text-[10px]">SUPERHOST</span>` 
            : ''
           }
-         <p>${tarjeta.type}. ${tarjeta.beds ??= ""} beds  </p>
+         <p>${tarjeta.type}. ${tarjeta.beds ?? ""} beds  </p>
           </div>
 
             </div>
@@ -46,4 +51,58 @@ for (const tarjeta of tarjet){
 
         </div>`
     contenedorTarjeta.innerHTML += tarjetasGeneradas;
+        }
+    
 }
+
+
+
+
+
+// FILTRO POR CIUDAD
+function filtrarCiudad() {
+
+    let ciudad = addL.value.toLowerCase().trim();
+
+
+    let resultado = tarjet.filter((lugar) =>
+
+        lugar.city.toLowerCase().includes(ciudad)
+
+    );
+    renderizarHtml(resultado)
+
+  
+    console.log(resultado)
+}
+
+
+addL.addEventListener("input", () => {
+
+    filtrarCiudad();
+
+    btnAl.textContent =
+        addL.value === ""
+            ? "Add location"
+            : addL.value;
+});
+console.log(filtrarCiudad)
+
+
+// ABRIR Y CERRAR MODAL
+let boton = document.querySelector("#boton");
+let dropdwn = document.querySelector("#dropdwn");
+
+boton.addEventListener("click", () => {
+    dropdwn.classList.toggle("hidden");
+});
+
+document.addEventListener("click", (e) => {
+
+    if (
+        !dropdwn.contains(e.target) &&
+        !boton.contains(e.target)
+    ) {
+        dropdwn.classList.add("hidden");
+    }
+});
